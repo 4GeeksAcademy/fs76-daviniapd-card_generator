4ee3a4a24f41a570740fc12f3534df16cf279ef0
Davinia pd <delgadavi@gmail.com>
2024-07-24 19:15:24 +0000

    funcion de cardGenerator creada y funcional

--- src/app.js
+++ src/app.js
@@ -1,11 +1,33 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+// function cardGenerator() {
+//   console.log("carta aleatoria");
+// }
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+// cardGenerator();
+function cardGenerator() {
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let suits = ["clubs", "diamonds", "hearts", "spades"];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  let randomNumberIndex = Math.floor(Math.random() * numbers.length);
+  let randomSuitIndex = Math.floor(Math.random() * suits.length);
+
+  let randomNumber = numbers[randomNumberIndex];
+  let randomSuit = suits[randomSuitIndex];
+
+  console.log(`Your card is ${randomNumber} of ${randomSuit}`);
+}
+
+cardGenerator();
