af90feec3d1a63a2db11f24d1170a6e58c94bf5f
Davinia pd <delgadavi@gmail.com>
2024-07-25 09:21:00 +0000

    agregados simbolos palos y reflejadas las cartas en el html

--- src/app.js
+++ src/app.js
@@ -14,15 +14,18 @@ function cardGenerator() {
     "Q",
     "K"
   ];
-  let suits = ["clubs", "diamonds", "hearts", "spades"];
+  let suits = ["♦", "♥", "♠", "♣"];
 
-  let randomNumberIndex = Math.floor(Math.random() * numbers.length);
-  let randomSuitIndex = Math.floor(Math.random() * suits.length);
+  // let randomNumberIndex = Math.floor(Math.random() * numbers.length);
+  // let randomSuitIndex = Math.floor(Math.random() * suits.length);
 
-  let randomNumber = numbers[randomNumberIndex];
-  let randomSuit = suits[randomSuitIndex];
+  let randomNumber = numbers[Math.floor(Math.random() * numbers.length)];
+  let randomSuit = suits[Math.floor(Math.random() * suits.length)];
 
+  document.getElementById("numberCard").innerHTML = randomNumber;
+  document.getElementById("suitCardTop").innerHTML = randomSuit;
+  document.getElementById("suitCardBottom").innerHTML = randomSuit;
+
+  // eslint-disable-next-line no-console
   console.log(`Your card is ${randomNumber} of ${randomSuit}`);
 }
-
-cardGenerator();
