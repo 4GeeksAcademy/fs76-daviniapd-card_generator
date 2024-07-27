faaae4986d2ef67545822b10b5bd63a32eda3728
Davinia pd <delgadavi@gmail.com>
2024-07-27 11:48:14 +0000

    temporizador generado y hecha alerta con la info

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
+let alertShown = false;
+
 function cardGenerator() {
   let numbers = [
     "A",
@@ -35,4 +37,23 @@ function cardGenerator() {
     randomSuit === "♦" || randomSuit === "♥" ? "#dc3545" : "";
 
   console.log(`Your card is ${randomNumber} of ${randomSuit}`);
+
+  if (!alertShown) {
+    showAlert(
+      "If you don't like your card it will automatically generate another one every 10 seconds or you can press the button, too.",
+      "info"
+    );
+    alertShown = true;
+  }
 }
+
+function showAlert(message, type) {
+  const alertContainer = document.getElementById("alertContainer");
+  const alertDiv = document.createElement("div");
+  alertDiv.className = `alert alert-${type} alert-dismissible fade show`;
+  alertDiv.role = "alert";
+  alertDiv.innerHTML = `${message} <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>`;
+  alertContainer.appendChild(alertDiv);
+}
+
+setInterval(cardGenerator, 10000);
